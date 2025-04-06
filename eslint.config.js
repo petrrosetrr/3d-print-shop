4312216f34400e8a0e6fc794384b8386867c5237
Petr Podovalov <petr.podo@gmail.com>
2025-04-06 20:43:29 +0200

    фикс

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import ts from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import prettier from 'eslint-config-prettier'
 
+/** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
   js.configs.recommended,
   {
